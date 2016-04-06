c5a65ad8ea62c530d70bb250ba2c006b8d16028f
Dima Bugakov <dabugakov@Dimas-MacBook-Pro.local>
2016-04-06 20:20:48 +0300

    # Add thread

--- grabber/src/main/scala/com/dabugakov/grabber/Main.scala
+++ grabber/src/main/scala/com/dabugakov/grabber/Main.scala
@@ -11,9 +11,8 @@ import scala.collection.mutable.ArrayBuffer
 
 object Main {
 
-  def main(args: Array[String]) {
 
-    //    while (true) {
+  def main(args: Array[String]) {
 
     val (fileName, apiKey) = {
       val prop = new Properties()
@@ -24,63 +23,68 @@ object Main {
         )
     }
 
-    val cities: List[String] = List("Moscow,RU", "London", "Tokyo", "Kiev", "Amsterdam,NL",
-                                                    "Ankara", "Berlin", "Brussels", "Oslo", "Paris,FR")
-    val dataBuffer = ArrayBuffer.empty[String]
+    (new Worker(fileName, apiKey)).run()
 
-    for (city <- cities) {
-      Thread.sleep(1000)
-
-      val result = {
-        val httpClient = new DefaultHttpClient()
-        val httpResponse = httpClient.execute(new HttpGet("http://api.openweathermap.org/data/2.5/weather?q=" + city + apiKey));
-        val entity = httpResponse.getEntity()
-
-        var content = ""
-        if (entity != null) {
-          val inputStream = entity.getContent()
-          content = io.Source.fromInputStream(inputStream).getLines.mkString
-          inputStream.close
-        }
-        httpClient.getConnectionManager().shutdown()
-        Thread.sleep(1000)
-        content
-      }
-
-      val data = {
-        val fields = JsonParser(result).asJsObject.fields
-        val mainFields = fields.get("main").toList(0).asJsObject
-        val coord = fields.get("coord").toList(0).asJsObject
-        val preData = ArrayBuffer.empty[String]
-        preData += coord.getFields("lat")(0).toString()
-        preData += coord.getFields("lon")(0).toString()
-        preData += fields.get("dt").toList(0).toString()
-        preData += mainFields.getFields("pressure")(0).toString()
-        preData += mainFields.getFields("humidity")(0).toString()
-        preData += mainFields.getFields("temp")(0).toString()
-        Thread.sleep(1000)
-        preData.mkString(";")
-      }
-
-      dataBuffer += data
-
-    }
-
-
-    lazy val print = {
-      val printWriter = new PrintWriter(new FileOutputStream(new File(fileName), true))
-      printWriter
-    }
-
-    if (dataBuffer.length == 10) {
-      for (data <- dataBuffer) {
-        print.write(data + '\n')
-      }
-    }
-
-    print.close()
-    //    }
 
   }
 
+}
+
+class Worker(fileName: String, apiKey: String) extends Runnable {
+  def run(): Unit = {
+    while (true) {
+      val cities: List[String] = List("Moscow,RU", "London", "Tokyo", "Kiev", "Amsterdam,NL",
+        "Ankara", "Berlin", "Brussels", "Oslo", "Paris,FR")
+      val dataBuffer = ArrayBuffer.empty[String]
+
+      for (city <- cities) {
+        Thread.sleep(10000)
+
+        val result = {
+          val httpClient = new DefaultHttpClient()
+          val httpResponse = httpClient.execute(new HttpGet("http://api.openweathermap.org/data/2.5/weather?q=" + city + apiKey));
+          val entity = httpResponse.getEntity()
+
+          var content = ""
+          if (entity != null) {
+            val inputStream = entity.getContent()
+            content = io.Source.fromInputStream(inputStream).getLines.mkString
+            inputStream.close
+          }
+          httpClient.getConnectionManager().shutdown()
+          content
+        }
+
+        val data = {
+          val fields = JsonParser(result).asJsObject.fields
+          val mainFields = fields.get("main").toList(0).asJsObject
+          val coord = fields.get("coord").toList(0).asJsObject
+          val preData = ArrayBuffer.empty[String]
+          preData += coord.getFields("lat")(0).toString()
+          preData += coord.getFields("lon")(0).toString()
+          preData += fields.get("dt").toList(0).toString()
+          preData += mainFields.getFields("pressure")(0).toString()
+          preData += mainFields.getFields("humidity")(0).toString()
+          preData += mainFields.getFields("temp")(0).toString()
+          preData.mkString(";")
+        }
+
+        dataBuffer += data
+
+      }
+
+      lazy val print = {
+        val printWriter = new PrintWriter(new FileOutputStream(new File(fileName), true))
+        printWriter
+      }
+
+      if (dataBuffer.length == 100) {
+        for (data <- dataBuffer) {
+          print.write(data + '\n')
+        }
+      }
+
+      print.close()
+    }
+  }
 }
